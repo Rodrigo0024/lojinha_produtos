fa85f184589ba7adbc6c18c4b431797f704dc58a
rodrigo <24rodrigocurso@gmail.com>
2025-08-24 14:32:53 -0300

    herdando a classe bolacha

--- produtos.py
+++ produtos.py
@@ -5,3 +5,14 @@ class Bolacha:
         self.preco = preco
     def imprima(self):
         print(f"Marca: {self.marca} \n Sabor: {self.sabor} \n preço: {self.preco}")
+
+class Salgados(Bolacha):
+    def __init__(self, marca, sabor, preco, tipo):
+        # Chama o construtor da classe pai
+        super().__init__(marca, sabor, preco)
+        self.tipo = tipo  # atributo novo só da classe Salgos
+
+    # Pode usar os métodos herdados ou sobrescrever
+    def imprima(self):
+        super().imprima()  # reaproveita o da classe Bolacha
+        print(f"Tipo: {self.tipo}")
